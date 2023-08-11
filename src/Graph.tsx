0243dd2feb72a96818593c2948d32f81115c50e5
Daria Vekic <dav00002@students.stir.ac.uk>
2023-08-11 15:56:24 +0100

    Forage JPMC SWE Task 3

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -23,10 +23,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      price_abc: 'float', //required to calculate ratio
+      price_def: 'float', //required to calculate ratio
+      ratio: 'float', //to track two stocks' ratio rather than distinguishing between two stocks
+      timestamp: 'date', //the data we are tracking is with respect to time
+      upper_bound: 'float', //required to track when bounds are crossed
+      lower_bound: 'float', //required to track when bounds are crossed
+      trigger_alert: 'float', //the alert when upper_bound and lower_bound crosses
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -35,24 +38,30 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
+      elem.setAttribute('view', 'y_line'); //the kind of graph to visualise, set to y_line
+      elem.setAttribute('row-pivots', '["timestamp"]'); //responsible for x-axis, data tracked with respect to time
+      //'columns' limits the datapoint's data plotted along y-axis to fields specified
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      //'aggregates' eliminates duplicate data by consolidating duplicates into single datapoint.
+      //Datapoint is unique if it has timestamp; otherwise get avg of other non-unique fields that similar datapoints share
+      //before treating them as one.
+      elem.setAttribute('aggregates', JSON.stringify({ 
+        price_abc: 'avg',
+        price_def: 'avg', 
+        ratio: 'avg',
+        timestamp: 'distinct count', 
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
